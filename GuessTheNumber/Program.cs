18ab67c4e2e2b629e4c801e55aeaa43b2b6e3b61
BPPL0806 <84276537+BPPL0806@users.noreply.github.com>
2022-10-09 10:50:49 +0200

    Add files via upload

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace GuessTheNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int random_num = random.Next(1, 50); //Generate number 1-50
+            int tries = 0;
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Guess number from 1 to 50");
+                    int answer = Convert.ToInt32(Console.ReadLine());
+                    tries++;
+
+                    if (answer == random_num && tries == 1)
+                    {
+                        Console.WriteLine("Correct answer! It's " + random_num);
+                        Console.WriteLine("You guessed it at 1st try!");
+                        break;
+                    }
+
+                    else if (answer < random_num)
+                    {
+                        Console.WriteLine("Generated number is bigger than " + answer + ".");
+                        continue;
+                    }
+
+                    else if (answer > random_num)
+                    {
+                        Console.WriteLine("Generated number is smaller than " + answer + ".");
+                        continue;
+                    }
+
+                    if (answer == random_num)
+                    {
+                        Console.WriteLine("Correct answer! It's " + random_num + ".");
+                        Console.WriteLine("You guessed number at " + tries + " tries.");
+                        break;
+                    }
+                }
+
+                catch (Exception) //Error handling
+                {
+                    Console.WriteLine("It's not a number!");
+                    continue;
+                }
+            }
+        }    
+    }
+}
